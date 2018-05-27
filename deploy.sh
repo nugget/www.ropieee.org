821c87cac3a95f1f6f6971908252c324ab01cb3d
spockfish <htenberge@gmail.com>
2018-05-27 14:26:35 +0200

    small change to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # this script takes care of deploying the website
 
 
-hugo
+hugo --config config.yaml,config.metadata.yaml
 scp -P 1974 -r public/* root@vps.ten-berge.org:/srv/www/htdocs/www.ropieee.org
